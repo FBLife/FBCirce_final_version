d81053200f36b5902a2da5630baa108857efb8c6
fblife-zhang <zhangshaonan@fblife.com>
2014-06-23 11:52:34 +0800

    the final version for FBCircle
    you can only push and commit your class

---
+++ FBCircle/GM/C/GscanfViewController.h
@@ -0,0 +1,29 @@
+//
+//  GscanfViewController.h
+//  FBCircle
+//
+//  Created by gaomeng on 14-6-19.
+//  Copyright (c) 2014年 szk. All rights reserved.
+//
+
+
+//扫一扫vc
+#import <UIKit/UIKit.h>
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface GscanfViewController : UIViewController<AVCaptureMetadataOutputObjectsDelegate>
+
+{
+    int num;
+    BOOL upOrdown;
+    NSTimer * timer;
+}
+@property (strong,nonatomic)AVCaptureDevice * device;
+@property (strong,nonatomic)AVCaptureDeviceInput * input;
+@property (strong,nonatomic)AVCaptureMetadataOutput * output;
+@property (strong,nonatomic)AVCaptureSession * session;
+@property (strong,nonatomic)AVCaptureVideoPreviewLayer * preview;
+@property (nonatomic, retain) UIImageView * line;
+
+@end
